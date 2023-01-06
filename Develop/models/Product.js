255e6fd1b4c7d192ffa5ca785333b9384992a592
Andrew Connally <ajconnally93@gmail.com>
2023-01-06 13:17:16 -0600

    set up product.js model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,50 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    // CODE TO CREATE THE INITIAL PRODUCT MODEL - CAN BE EXTENDED and expanded upon
+    id: {
+      type: DataTypes.INTEGER,
+      alowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+
+      // setting allowNull to TRUE allows us to delete a category - setting to false for stock, price, product_name, and of course Id
+      allowNull: true,
+
+      references: {
+        model: "category",
+        key: "id",
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
