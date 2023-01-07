045de5991ccc229daa25809033b0b34aa3f59d89
Andrew Connally <ajconnally93@gmail.com>
2023-01-07 13:45:15 -0600

    set up category.js model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,8 +6,25 @@ class Category extends Model {}
 
 Category.init(
   {
+
     // define columns
+
+    // setting up auto-ID will be a very similar process every time
+    // setting up models in general will follow a lot of the same rules every time
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
+
+
   {
     sequelize,
     timestamps: false,
